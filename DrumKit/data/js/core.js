a7e6143e89181d6061d3f8447eab49f0e608c7f6
Sascha K <ku3menko.sasha@gmail.com>
2018-12-12 18:35:34 +0200

    refactoring

--- DrumKit/data/js/core.js
+++ DrumKit/data/js/core.js
@@ -11,29 +11,31 @@ var sounds = {
 }
 
 var handlers = {
+    removeTransition: function (e) {
+        if (e.propertyName !== 'transform');
+        this.classList.remove('playing');
+    },
+    playSound: function (event) {
+        if (sounds[event.keyCode]) {
+            let audio = new Audio(sounds[event.keyCode]);
+            audio.play();
+            let key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+            key.classList.add('playing');
+        }  
+    },
+    setupEventListeners: function() {
+        const keys = document.querySelectorAll('.key');
+        keys.forEach(a => a.addEventListener('transitionend', handlers.removeTransition));
+        window.addEventListener('keydown', handlers.playSound);
+    }
     
 }
 
-function removeTransition(e) {
-    if (e.propertyName !== 'transform');
-    this.classList.remove('playing');
-}
+handlers.setupEventListeners();
 
-function playSound(event) {
-    if (sounds[event.keyCode]) {
-        let audio = new Audio(sounds[event.keyCode]);
-        audio.play();
-        let key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-        key.classList.add('playing');
-    }  
-}
 
-buttonDiv = document.querySelector('.keys_container');
-const keys = document.querySelectorAll('.key');
-var events = {
-    
-}
-keys.forEach(a => a.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+
+
+
 
 
