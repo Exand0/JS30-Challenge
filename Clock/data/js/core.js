0c7b56040f34707ea110afcdb2dd275b9195dba9
Sascha K <ku3menko.sasha@gmail.com>
2018-12-13 19:23:29 +0200

    clock customizations

--- Clock/data/js/core.js
+++ Clock/data/js/core.js
@@ -1,29 +1,39 @@
-const clock_div = document.querySelector('.clock');
+const watchfaceDiv = document.querySelector('.watchface');
 let numbers = [12, 3, 6, 9];
 for (i = 0; i < 4; i++) {
     let digits = document.createElement('div');
     digits.classList.add('digits', `number${numbers[i]}`);
     digits.textContent = numbers[i];
-    clock_div.appendChild(digits);
+    watchfaceDiv.appendChild(digits);
 }
 
-function moveClockHands() {
-    let currentDate = new Date;
-    let h = 30 * (currentDate.getHours()%12) + (currentDate.getMinutes()/60);
-    let m = 6 * currentDate.getMinutes();
-    let s = 6 * currentDate.getSeconds();
+function moveClockHands(date) {
+    date = new Date;
+    let hands = [
+        {
+            hand: 'hours',
+            angle: 30 * (date.getHours()%12) + (date.getMinutes()/60)
+        },
+        {
+            hand: 'minutes',
+            angle: 6 * date.getMinutes()
+        },
+        {
+            hand: 'seconds',
+            angle: s = 6 * date.getSeconds()
+        }
+    ]
+    for (element of hands) {
+        let hand = element.hand;
+        let handDiv = document.querySelector(`.${hand}`);
+        handDiv.style.cssText = `transform:rotate(${element.angle}deg);`;
 
-    let hourHand = document.querySelector('.hours');
-    let minuteHand = document.querySelector('.minutes');
-    let secondHand = document.querySelector('.seconds');
-
-    hourHand.style.cssText = `transform:rotate(${h}deg);`;
-    minuteHand.style.cssText = `transform:rotate(${m}deg);`;
-    secondHand.style.cssText = `transform:rotate(${s}deg);`;
-
+    }
     setTimeout(moveClockHands, 1000);
 }
 
-window.addEventListener('load', moveClockHands);
+
+let date = new Date;
+window.addEventListener('load', moveClockHands(date));
 
 //moveClockHands();
