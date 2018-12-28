a19a263adb0a9aa2238f8e4baa0235572c1b5786
Sascha K <ku3menko.sasha@gmail.com>
2018-12-28 20:59:09 +0200

    finished + first refactoring

--- VideoPlayer/data/js/core.js
+++ VideoPlayer/data/js/core.js
@@ -1,58 +1,67 @@
 let supported = document.createElement('video').canPlayType;
+
 if (supported) {
-    console.log('test');
+    
     let player = document.querySelector('.player');
-    let video = document.querySelector('video');
-    let mediaControl = document.querySelector('.media-control');
+    let video = player.querySelector('video');
+    let mediaControl = player.querySelector('.media-control');
 
     video.controls = false;
     mediaControl.style.display = 'block';
 
-    let playPause = document.querySelector('#toggle');
-    let mute = document.querySelector('#mute');
-    let forward = document.querySelector('#forward');
-    let backward = document.querySelector('#backward');
-    let progress = document.querySelector('#progress');
-    let progressBar = document.querySelector('#progress-bar');
-    let vol = document.querySelector('#vol');
-    let full = document.querySelector('#full');
+    let playPause = player.querySelector('#toggle');
+    let muteButton = player.querySelector('#mute');
+    let skipButtons = player.querySelectorAll('[data-skip]');
+    let progress = player.querySelector('#progress');
+    let progressBar = player.querySelector('#progress-bar');
+    let vol = player.querySelector('#vol');
+    // let full = document.querySelector('#full');
 
-    playPause.addEventListener('click', e => {
-        if (video.paused || video.ended) {
-            video.play();
-            playPause.classList.add('fa-stop');
-            playPause.classList.remove('fa-play');
-        } else {
-            video.pause();
-            playPause.classList.add('fa-play');
-            playPause.classList.remove('fa-stop');
-            
-        }
-    })
+    function toggleVideo() {
+        let method = (video.paused || video.ended) ? 'play' : 'pause';
+        video[method]();
+        playPause.classList.toggle('fa-stop');
+        playPause.classList.toggle('fa-play');
 
-    mute.addEventListener('click', e => {
+    }
+
+    function mute() {
         video.muted = !video.muted;
-        if (video.muted) {
-            mute.classList.remove('fa-volume-up');
-            mute.classList.add('fa-volume-mute');
-        } else {
-            mute.classList.remove('fa-volume-mute');
-            mute.classList.add('fa-volume-up');  
-        }
-    });
+        muteButton.classList.toggle('fa-volume-up');
+        muteButton.classList.toggle('fa-volume-mute');
+    }
 
-    vol.addEventListener('input', e => {
-        //vol.value = video.volume;
-        console.log(vol.value);
+    function setVol() {
         video.volume = vol.value/100;
-    });
+        if (video.muted && video.volume > 0) mute();
+    }
 
-    video.addEventListener('timeupdate', () => {
-        // progress.value = video.currentTime;
-        // progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100) + '%';
+    function updateTimeBar() {
+        progress.max = video.duration;
         progress.value = video.currentTime;
-        // progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100) + '%';     
-    })
+        // fallback option
+        progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100) + '%';
+    }
+
+    function setTime(e) {
+        const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+        video.currentTime = scrubTime;
+    }
+
+    function skipTime() {
+        video.currentTime += parseInt(this.dataset.skip);
+    }
+
+    playPause.addEventListener('click', toggleVideo);
+    muteButton.addEventListener('click', mute);
+    vol.addEventListener('input', setVol);
+    video.addEventListener('timeupdate', updateTimeBar);
+    progress.addEventListener('click', setTime);
+    skipButtons.forEach(button => button.addEventListener('click', skipTime));
+ 
+    // fullscreen support
+    // var fullScreenSupported = !!(document.fullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled || document.webkitSupportsFullscreen || document.webkitFullscreenEnabled || document.createElement('video').webkitRequestFullScreen);
+
 }
 
 
