16aaa1bdd33b2dfaeaafcd00c86b1ac7dcfc651c
Sascha K <ku3menko.sasha@gmail.com>
2019-01-06 19:29:06 +0200

    new js added

--- LocalStorage/data/js/core.js
+++ LocalStorage/data/js/core.js
@@ -1,11 +1,42 @@
 let container = document.querySelector('.container');
 let form = container.querySelector('.add');
 let inputText = container.querySelector('.text-input');
-
+let ul = container.querySelector('ul');
 
 function addEntry(e) {
     e.preventDefault();
-    console.log(e);
+    let listEl = document.createElement('li');
+    let label = document.createElement('label');
+    let checkbox = document.createElement('input');
+
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.classList.add('checkbox');
+
+    label.textContent = inputText.value;
+    inputText.value = '';
+
+    listEl.innerHTML += checkbox.outerHTML + label.outerHTML;
+    ul.appendChild(listEl); 
+
+    addToStorage('ul', ul.outerHTML);
+    console.log(localStorage.getItem('ul'));
+}
+
+function addToStorage(key, value) {
+    localStorage.setItem(key, value);
+}
+
+function getFromStorage(key) {
+    // console.log(localStorage.getItem(key))
+    return localStorage.getItem(key);
+}
+
+function checkData() {
+    if (getFromStorage) {
+        ul = getFromStorage('ul');
+        // console.log('ul');
+    }
 }
 
 form.addEventListener('submit', addEntry);
+// checkData()
