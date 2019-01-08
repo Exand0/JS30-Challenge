68f20926493e1e5ca3e3165e9930bd40b1c04ec8
Sascha K <ku3menko.sasha@gmail.com>
2019-01-08 22:11:29 +0200

    finished /w horrible readability / refactoring needed

--- LocalStorage/data/js/core.js
+++ LocalStorage/data/js/core.js
@@ -4,31 +4,41 @@ let inputText = container.querySelector('.text-input');
 let ul = container.querySelector('ul');
 let resetButton = container.querySelector('.reset-button');
 let data = [];
+let arrIndex = 0;
 
 function buildEntry(e) {
+    console.log(arrIndex);
     e.preventDefault();
     let listEl = document.createElement('li');
     let label = document.createElement('label');
     let checkbox = document.createElement('input');
-
     checkbox.setAttribute('type', 'checkbox');
     checkbox.classList.add('checkbox');
 
     label.textContent = inputText.value;
     inputText.value = '';
     listEl.innerHTML += checkbox.outerHTML + label.outerHTML;
+    // listEl.dataset.seq = i;
     // local storage is constantly overwritten
-    data.push(JSON.stringify(listEl.outerHTML));
+    data[0] = listEl.outerHTML;
     addEntry();
 
 }
 
 function addEntry() {
+    // Separate/optimize this function for usage for both initial addition of entries
+    // end for checLocalStorage
     data.forEach(li => {
         // console.log(JSON.parse(li));
-        ul.innerHTML += (JSON.parse(li)); 
+        ul.innerHTML += li; 
+        addToStorage(`li${arrIndex}`, li);
+        // console.log(getFromStorage(`li${arrIndex}`)); 
+        // console.log(getFromStorage(`arrIndex`)); 
+
+        addToStorage('arrIndex', arrIndex);
+        arrIndex++;
     })
-    addToStorage('ulData', data);
+    // addToStorage('ulData', data);
     data = [];
     // console.log(localStorage.getItem('ulData'));
 }
@@ -38,23 +48,30 @@ function addToStorage(key, value) {
 }
 
 function getFromStorage(key) {
-    console.log(localStorage.getItem(key))
     return localStorage.getItem(key);
 }
 
 function checkLocalStorage() {
-    if (getFromStorage('ulData')) {
-        data = localStorage.getItem('ulData');
-        console.log(data.length);
-        // Data is not an array anymore
+    if (getFromStorage('arrIndex')) {
+        for (let i=(getFromStorage('arrIndex')); i >= 0; i--) {
+            data[i] = getFromStorage(`li${i}`);  
+            // console.log(data[i]);
+        }
         addEntry(data);
-        data = [];
     }
 }
 
 function resetData() {
     data = [];
+    if (arrIndex = getFromStorage('arrIndex')) {
+        for (let i=arrIndex; i >= 0; i--) {
+            // console.log(getFromStorage(`li${i}`));
+            localStorage.removeItem(`li${i}`);
+        }
+    }
     localStorage.removeItem('ulData');
+    localStorage.removeItem('arrIndex');
+    arrIndex = 0;
 }
 
 form.addEventListener('submit', buildEntry);
