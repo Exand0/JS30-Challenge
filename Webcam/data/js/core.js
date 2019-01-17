894f30fdeb483632b5fc6e62c1f9685154459b47
Sascha K <ku3menko.sasha@gmail.com>
2019-01-17 21:42:01 +0200

    finished

--- Webcam/data/js/core.js
+++ Webcam/data/js/core.js
@@ -1,6 +1,7 @@
 let photoboothDiv = document.querySelector('.photobooth');
 let photoButton = photoboothDiv.querySelector('.take-photo');
 let cameraStreamDiv = photoboothDiv.querySelector('.camera-stream');
+let body = document.querySelector('body');
 let video = document.createElement('video');
 let w, h, ratio;
 
@@ -13,26 +14,6 @@ navigator.mediaDevices.getUserMedia({video: {width: 1920, height: 1080}})
         })
     .catch(err => console.log(`${err.name}: ${err.message}`));
 
-    // function snap() {
-    //     // Define the size of the rectangle that will be filled (basically the entire element)
-    //     context.fillRect(0, 0, w, h);
-    //     // Grab the image from the video
-    //     context.drawImage(video, 0, 0, w, h);
-    // }
-
-
-    // video.addEventListener('loadedmetadata', function() {
-    //     // Calculate the ratio of the video's width to height
-    //     ratio = video.videoWidth / video.videoHeight;
-    //     // Define the required width as 100 pixels smaller than the actual video's width
-    //     w = video.videoWidth - 100;
-    //     // Calculate the height based on the video's width and the ratio
-    //     h = parseInt(w / ratio, 10);
-    //     // Set the canvas width and height to the values just calculated
-    //     canvas.width = w;
-    //     canvas.height = h;			
-    // }, false);
-
 function takeAndAddScreenshot() {
     addScreenshotToHtml(takeScreenshot(video));
 }
@@ -44,11 +25,10 @@ function takeAndAddScreenshot() {
 function takeScreenshot(videoEl) {
     let canvas = document.createElement('canvas');
     let ctx = canvas.getContext('2d');
-    // canvas.width = canvas.width*ratio;
-    // canvas.height = canvas.width/ratio;
-    ctx.fillRect(0,0, w/3, h/3);
-    ctx.drawImage(videoEl, 0,0, w/3, h/3);
-    // console.log((ratio));
+    canvas.width = w/4;
+    canvas.height = h/4;
+    ctx.fillRect(0,0, w/4, h/4);
+    ctx.drawImage(videoEl, 0,0, w/4, h/4);
     return canvas;
 }
 
@@ -57,20 +37,38 @@ function getVideoRatio(e) {
     w = videoEl.videoWidth;
     ratio = w/videoEl.videoHeight;
     h = parseInt(w/ratio);
-    // console.log(e.target); 
-    // console.log(w,h);
-    // console.log(ratio);
 }
 
-function addScreenshotToHtml(screenshot) {
+function addScreenshotToHtml(canvasScreenshot) {
     let div = photoboothDiv.querySelector('.screenshot-container');
     if (!div) {
         div = document.createElement('div');
         div.classList.add('screenshot-container');
     }
+    // document.documentElement.style.setProperty('--degree', `${randomDegrees()}deg`);
+    // canvasScreenshot.style.setProperty('--degree', `${randomDegrees()}deg`);
+    canvasScreenshot.style.setProperty('transform', `rotate(${randomDegrees()}deg)`);
     photoboothDiv.appendChild(div);
-    div.appendChild(screenshot);  
+    div.appendChild(canvasScreenshot);  
 }
+
+function randomDegrees() {
+    let deg = Math.floor(Math.random()*31);
+    let prefix = (Math.floor(Math.random() * 3) === 1) ? '' : '-' ;
+    console.log(prefix+deg)
+    return (prefix + deg);
+}
+
+console.log(randomDegrees());
+
+
 
 photoButton.addEventListener('click', takeAndAddScreenshot);
 video.addEventListener('loadedmetadata', getVideoRatio);
+// body.addEventListener('change', () => {
+//     body.style.backgroundSize = cover;
+// });
+// body.addEventListener('scroll', () => {
+//     body.style.backgroundSize = cover;
+// });
+
