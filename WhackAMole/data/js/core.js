34194190b90ab0e1b05127f2f22d7c3b6335d4c0
Sascha K <ku3menko.sasha@gmail.com>
2019-02-13 22:26:42 +0200

    finished

--- WhackAMole/data/js/core.js
+++ WhackAMole/data/js/core.js
@@ -93,7 +93,6 @@ function spawnMoles() {
 }
 
 function getRandomTime(min, max) {
-    console.log(min, max);
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
